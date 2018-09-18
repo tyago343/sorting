af253ccfd7cf00a1a51f1a9aa49e3887ed9bbc10
tyago343 <tyagocasanova@hotmail.com>
2018-09-18 12:46:06 -0300

    inicializando repositorio

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,6 +1,6 @@
 describe('Split Array function', function() {
   it('es capar de dividir el arreglo en dos partes', function() {
-    
+    expect(split([1,5,8,2,9,3])).toEqual([[1,5,8],[2,9,3]]);
   });
     
 });
